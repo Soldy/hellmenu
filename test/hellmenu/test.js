17ae0ff0658f5f3202026516e0409ec87e8b8179
Soldy <4786022+Soldy@users.noreply.github.com>
2022-07-17 21:42:56 +0100

    example test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ test/hellmenu/test.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+    hellMenu = new hellMenuClass();
+    hellMenu.setMenu(
+        'test1',
+        'Test menu 1',
+        'test_icon_1',
+        [],
+        'main'
+    );
+}
